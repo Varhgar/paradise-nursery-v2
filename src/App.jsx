dce86c392cc97cfa6273d774ea1f637cd4cc04a4
Varhgar <rmatthies39@gmail.com>
2025-08-25 17:13:27 -0500

    added files to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import ProductPage from './pages/ProductPage/ProductPage';
 import CartPage from './pages/CartPage/CartPage';
 
 function App() {
   return (
-    <Router>
+    <Router basename='/paradise-nursery-v2'>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/products" element={<ProductPage />} />
