13562c6cc4d4da5b20048db6cbb2f3303ad68ec9
Varhgar <rmatthies39@gmail.com>
2025-08-25 18:42:44 -0500

    Changed to HashRoute for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import ProductPage from './pages/ProductPage/ProductPage';
 import CartPage from './pages/CartPage/CartPage';
 
 function App() {
   return (
-    <Router basename='/paradise-nursery-v2'>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/products" element={<ProductPage />} />
         <Route path="/cart" element={<CartPage />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
